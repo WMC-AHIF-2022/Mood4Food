ea11c32a5799adc77dd51494b60cec3c7c8cbe66
Brudermichael <Michael.Werner@24speed.at>
2023-05-03 10:52:03 +0200

    worked on Order Site design

---
+++ Code/src/client/pages/OrderSite/orderSite.ts
@@ -0,0 +1,16 @@
+import {Food} from "../../../server/collective/Food";
+import {Person} from "../../../server/collective/Person";
+import {OrderEntry} from "../../../server/collective/Order"
+
+export function  OrderFood(): void{
+
+
+}
+
+
+/*
+von not orderd yet auf  
+
+
+
+*/
