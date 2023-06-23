6f17c13d48730eb09f46f351ee75a1b4b75fbecc
Yannik <ylisa0102@gmail.com>
2023-06-23 15:22:59 +0200

    updated export and removed consolelogs

--- Code/src/server/router/order-day-router.ts
+++ Code/src/server/router/order-day-router.ts
@@ -53,7 +53,7 @@ orderDayRouter.get("/foodAndAmount/:id", async (req, res)=>{
 orderDayRouter.post("/", async(req, res) => {
     const orderdate: any = req.body.orderdate;
     const deadline: any = req.body.deadline;
-    console.log(orderdate,deadline);
+    //console.log(orderdate,deadline);
     if (typeof orderdate !== "string" || orderdate.trim().length === 0) {
         res.status(StatusCodes.BAD_REQUEST).send("date missing or not ok");
         return;
@@ -70,7 +70,7 @@ orderDayRouter.post("/", async(req, res) => {
     const result1 = await stmt1.get();
     await stmt1.finalize();
     if(result1.count != 0){
-        console.log("that's not the first one");
+        //console.log("that's not the first one");
         const stmt2 = await db.prepare('select id as count from orderday order by 1 desc limit 1');
         const result2 = await stmt2.get();
         await stmt2.finalize();
@@ -93,7 +93,7 @@ orderDayRouter.post("/", async(req, res) => {
         result1.count = result2.count++;
     }
     */
-    console.log(id);
+    //console.log(id);
 
     // standard process
     const stmt = await db.prepare('insert or ignore into OrderDay values (?1, ?2, ?3)');
@@ -104,7 +104,7 @@ orderDayRouter.post("/", async(req, res) => {
     await stmt.finalize();
     await db.close();   
 
-    console.log(operationResult.changes);
+    //console.log(operationResult.changes);
     if(operationResult.changes == null || operationResult.changes !== 1){
         res.status(StatusCodes.BAD_REQUEST).send("db error occurred");
     }
