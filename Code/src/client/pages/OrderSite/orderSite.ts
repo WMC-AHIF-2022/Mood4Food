29e83109dcbab99359dcf24b627ddb59ba2e5664
Brudermichael <Michael.Werner@24speed.at>
2023-06-23 12:02:25 +0200

    designed OrderdaySite

--- Code/src/client/pages/OrderSite/orderSite.ts
+++ Code/src/client/pages/OrderSite/orderSite.ts
@@ -179,24 +179,3 @@ var gsDayNames = [
     'Saturday'
   ];
 
-
-
-/*
-
-Wenn man auf den not orderd yet knopf klickt, dann sollte man auf eine neue seite kommen. 
-Auf dieser Seite kann man dann für den jeweiligen orderday ein Essen auswählen. 
-Dann wird man automatisch wieder zu Orderday seite Zurückgeschupft.
-
-
-Wenn man schon ein Essen ausgewählt hat und die Deadline noch nicht erreicht ist, dann kann man sein Essen noch bearbeiten. 
-Dann ist es der gleiche Prozess wie oben. 
-
-
-Aus der Teacher Sicht, kann man keine Essen bestellen, sondern nur die jeweiligen Orderdays festelgen. 
-Dafür klickt man auf den [+ add Orderday] Button wo man auch auf eine Seperate Seite kommt.
-Da kann man einen Orderday erstellen und eine Deadline festelegen. 
-
-
-
-*/
-
